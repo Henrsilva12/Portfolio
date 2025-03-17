217dca5d5303b895d7d15a863ea1531851338d7d
Henrique Silva <113844247+Henrsilva12@users.noreply.github.com>
2025-03-16 22:27:16 -0300

    Alteracao na rota do envio de email

--- src/app/api/send-email/route.ts
+++ src/app/api/send-email/route.ts
@@ -9,7 +9,7 @@ export async function POST(req: Request) {
   if (!name || !email || !message) {
     return NextResponse.json({ error: "Todos os campos são obrigatórios!" }, { status: 400 });
   }
-
+/*
   // Configuração do Mailtrap
   const transporter = nodemailer.createTransport({
     host: "sandbox.smtp.mailtrap.io", // Servidor SMTP do Mailtrap
@@ -19,6 +19,17 @@ export async function POST(req: Request) {
       pass: "29e21d8c123f1e", // Sua senha do Mailtrap
     },
   });
+*/
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.EMAIL_USER, // Acessando o e-mail do .env
+        pass: process.env.EMAIL_PASSWORD, // Acessando a senha do .env
+      },
+      tls: {
+        rejectUnauthorized: false, // Ignora a verificação do certificado
+      },
+    });
 
   try {
     await transporter.sendMail({
