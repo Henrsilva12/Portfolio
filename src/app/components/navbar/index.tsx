e5262257e108ce967b23d554e32defd2a03e4c5b
Henrsilva12 <henrsilvasantos@gmail.com>
2025-07-23 19:37:49 -0300

    feat: Adiciona hover no navbar

--- src/app/components/navbar/index.tsx
+++ src/app/components/navbar/index.tsx
@@ -1,9 +1,10 @@
 "use client";
-
+import { useEffect, useState } from "react";
 import React from "react";
 import "./styles.css";
 
 const Navbar: React.FC = () => {
+  const [activeSection, setActiveSection] = useState("home");
 
   const sections = [
     { id: "home", label: "Início" },
@@ -12,7 +13,31 @@ const Navbar: React.FC = () => {
     { id: "contato", label: "Contato" },
   ];
 
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY + 100;
+
+      sections.forEach((section) => {
+        const element = document.getElementById(section.id);
+        if (element) {
+          const offsetTop = element.offsetTop;
+          const offsetHeight = element.offsetHeight;
+
+          if (scrollPosition >= offsetTop && scrollPosition < offsetTop + offsetHeight) {
+            setActiveSection(section.id);
+          }
+        }
+      });
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [sections]);
+
   const scrollToSection = (id: string) => {
+    setActiveSection(id);
     const section = document.getElementById(id);
     if (section) {
       section.scrollIntoView({ behavior: "smooth" });
@@ -23,10 +48,12 @@ const Navbar: React.FC = () => {
     <nav className="navbar">
       <ul className="nav-links">
         {sections.map((section) => (
-          <li key={section.id}>
-            <button type="button" className="nav-buton" onClick={() => scrollToSection(section.id)}
-              aria-label={`Ir para a seção ${section.label}`}>
+          <li key={section.id} className="nav-item">
+            <button type="button" className={`nav-button ${activeSection === section.id ? "active" : ""}`} 
+            onClick={() => scrollToSection(section.id)}
+            aria-label={`Ir para a seção ${section.label}`}>
               {section.label}
+              <span className="underline"></span>
             </button>
           </li>
         ))}
