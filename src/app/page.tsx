2079853c59e9df4aeefa35efe82b21653cb86024
Henrsilva12 <henrsilvasantos@gmail.com>
2025-07-26 21:37:26 -0300

    feat: Atualiza estrutura do portfolio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ import Footer from "./components/footer";
 import Projects from "./components/projetos";
 import ScrollButton from "./components/button";
 import { useTypewriter, Cursor } from "react-simple-typewriter";
+import { FaEnvelope, FaGithub, FaLinkedin, FaWhatsapp } from "react-icons/fa";
 
 
 export default function Home() {
@@ -25,8 +26,8 @@ export default function Home() {
       <section id="home" className="header-section">
         <div className="titles-photo">
           <div className="titles">
-            <h1>Henrique</h1>
-            <h2>Santos</h2>
+            <h1>Óla, eu sou o</h1>
+            <h2>Henrique Santos</h2>
             <div className="skills-typewriter">
               <h3 className="typewriter-text">
                 <span className="typewriter-span">
@@ -36,6 +37,21 @@ export default function Home() {
                   <Cursor />
                 </span>
               </h3>
+            </div>
+
+            <div className="social-icons">
+                <a href="https://www.linkedin.com/in/henrique-santos-4b7901287" target="_blank" rel="noopener noreferrer">
+                    <FaLinkedin className="social-icon" />
+                </a>
+                <a href="https://github.com/Henrsilva12" target="_blank" rel="noopener noreferrer">
+                  <FaGithub className="social-icon" />
+                </a>
+                 <a href="mailto:henrsilvasantos@gmail.com" target="_blank" rel="noopener noreferrer">
+                    <FaEnvelope className="contact-icon" />
+                </a>
+                <a href="https://wa.me/5577998986939" target="_blank" rel="noopener noreferrer">
+                  <FaWhatsapp className="contact-iconWhatsapp" />
+                </a>
             </div>
           </div>
 
@@ -52,32 +68,23 @@ export default function Home() {
       {/* Seção Sobre */}
       <section id="sobre" className="section-about">
         <h1>Sobre Mim</h1>
-        <p>
-          Sou um profissional apaixonado por design e desenvolvimento, com experiência em
-          criar soluções inovadoras e centradas no usuário. Minha abordagem combina
-          estética e funcionalidade, garantindo que cada projeto atenda às necessidades
-          do cliente e proporcione uma experiência excepcional ao usuário.
-        </p>
+          <p className="background-about">
+            Atuo com UI/UX Design, desenvolvimento front-end e gestão de projetos, 
+            transformando ideias em soluções digitais intuitivas e eficientes. 
+            Tenho experiência em criar interfaces que aliam estética e funcionalidade, 
+            sempre com foco na experiência do usuário e nos objetivos do projeto. Cada entrega 
+            é pensada para comunicar com clareza, encantar visualmente e resolver problemas reais de forma estratégica.
+          </p>
       </section>
 
         <section id="projetos" className="section-projects">
           <h1>Projetos</h1>
-          <p>
-            Exploro criatividade e funcionalidade em cada projeto que desenvolvo.
-            Aqui estão algumas <br /> iniciativas que destacam minha experiência em UI/UX
-            design, gestão de projetos ágeis e <br /> desenvolvimento de soluções
-            inovadoras.
-          </p>
           <Projects />
       </section>
 
       <section id="contato" className="section-contact">
           <h1>Contate me</h1>
-          <p>
-          Ficarei feliz em conversar sobre projetos e oportunidades. <br /> Entre em
-          contato!
-        </p>
-            <Contact />
+          <Contact />
       </section>
 
       <div className="footer">
